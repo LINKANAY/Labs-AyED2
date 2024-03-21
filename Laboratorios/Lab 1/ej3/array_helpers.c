34b078c019134b86e6ae18a0061b544ee2b5488b
Sebastian Sierra <61486646+LINKANAY@users.noreply.github.com>
2024-03-21 18:07:53 -0300

    Update

--- Laboratorios/Lab 1/ej3/array_helpers.c
+++ Laboratorios/Lab 1/ej3/array_helpers.c
@@ -44,3 +44,37 @@ void array_dump(int a[], unsigned int length) {
     }
     printf(" ]\n");
 }
+
+void print_help(char *program_name) {
+    /* Print the usage help of this program. */
+    printf("Usage: %s <input file path>\n\n"
+           "Loads an array given in a file in disk and prints it on the screen."
+           "\n\n"
+           "The input file must have the following format:\n"
+           " * The first line must contain only a positive integer,"
+           " which is the length of the array.\n"
+           " * The second line must contain the members of the array"
+           " separated by one or more spaces. Each member must be an integer."
+           "\n\n"
+           "In other words, the file format is:\n"
+           "<amount of array elements>\n"
+           "<array elem 1> <array elem 2> ... <array elem N>\n\n",
+           program_name);
+}
+
+char *parse_filepath(int argc, char *argv[]) {
+    /* Parse the filepath given by command line argument. */
+    char *result = NULL;
+    // Program takes exactly two arguments
+    // (the program's name itself and the input-filepath)
+    bool valid_args_count = (argc == 2);
+
+    if (!valid_args_count) {
+        print_help(argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    result = argv[1];
+
+    return result;
+}
