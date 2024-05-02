ddfaab73e561629dc8510eef2e4784c1d301dd72
Sebastian Sierra <sebastian.sierra@mi.unc.edu.ar>
2024-05-02 01:04:40 -0300

    update lab 4

---
+++ Laboratorios/Lab 4/ej2/swap.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void swap(int *a, int *b)
+{
+    int aux = *a;
+    printf("aux %d\n", aux);
+    *a = *b;
+    *b = aux;
+}
+
+int main()
+{
+    int x;
+    int y;
+    x = 6;
+    y = 4;
+    printf("'x:' %d, 'y:' %d\n", x, y);
+    swap(&x, &y);
+    printf("'x:' %d, 'y:' %d\n", x, y);
+
+    return (EXIT_SUCCESS);
+}
