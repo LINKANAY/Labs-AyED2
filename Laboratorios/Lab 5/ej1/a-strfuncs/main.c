e7b782b8ef35abc2bb32c1d37b2822b1251e3a0f
Sebastian Sierra <sebastian.sierra@mi.unc.edu.ar>
2024-05-13 01:15:07 -0300

    update lab5

--- Laboratorios/Lab 5/ej1/a-strfuncs/main.c
+++ Laboratorios/Lab 5/ej1/a-strfuncs/main.c
@@ -3,12 +3,12 @@
 
 #include "strfuncs.h"
 
-
-int main(void) {
-    char *some_str="h.o.l.a m.u.n.d.o.!";
-    char *some_symmetric="abcba";
-    char *some_asymmetric="ab";
-    char *filtered=NULL;
+int main(void)
+{
+    char *some_str = "h.o.l.a m.u.n.d.o.!";
+    char *some_symmetric = "abcba";
+    char *some_asymmetric = "abccba";
+    char *filtered = NULL;
 
     filtered = string_filter(some_str, '.');
     printf("original: '%s' (%ld)\n"
@@ -17,12 +17,11 @@ int main(void) {
            "La cadena '%s' resulta %s simétrica\n",
            some_str, string_length(some_str),
            filtered, string_length(filtered),
-           some_symmetric,  string_is_symmetric(some_symmetric) ? "ser": "NO ser",
-           some_asymmetric, string_is_symmetric(some_asymmetric) ? "ser": "NO ser");
+           some_symmetric, string_is_symmetric(some_symmetric) ? "ser" : "NO ser",
+           some_asymmetric, string_is_symmetric(some_asymmetric) ? "ser" : "NO ser");
 
     free(filtered);
-    filtered=NULL;
+    filtered = NULL;
 
     return EXIT_SUCCESS;
 }
-
